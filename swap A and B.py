ac1dbd96fed9dc81c574ad9fac77801768069679
unknown <junaidk1727@gmail.com>
2023-11-14 15:25:24 +0400

    swap between two variable

---
+++ swap A and B.py
@@ -0,0 +1,20 @@
+a = 25
+b = 30
+a,b = b,a
+print("First Way: ", a, b)
+
+
+x= 60
+y= 70
+temp = x
+x = y
+y = temp
+print("Secound Way: ", x, y)
+
+
+a = 11
+b = 7
+a = a + b
+b = a - b
+a = a - b
+print("Third Way: ", a, b)
