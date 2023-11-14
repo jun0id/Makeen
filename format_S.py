85d9db62ce23c141711d6456343cbba2ddae96ec
unknown <junaidk1727@gmail.com>
2023-11-14 15:32:27 +0400

    Format Specify

---
+++ format_S.py
@@ -0,0 +1,11 @@
+year = int(input('When you born? '))
+x= 2023
+age= x-year
+print("your age is: ", age)
+
+
+n = input('When you name? ')
+a = int(input('When you age? '))
+
+print("%-8s"%(n), "%8d"%(a))
+
